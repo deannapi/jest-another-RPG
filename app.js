7f10ade4e16c64a82dacef2caa8c0739fd7da46d
DeAnna Martinez <deannapi.mart@gmail.com>
2020-05-21 20:32:47 -0500

    added potion attack choices

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
 const Game = require('./lib/Game');
 
 new Game().initializeGame();
-
-inquirer
-    .prompt({
-        type: 'text',
-        name: 'name',
-        message: 'What is your name?'
-    })
-    // destructure name from the prompt object
-    .then(({ name }) => {
-        this.player = new Player(name);
-
-        // test the object creation
-        console.log(this.currentEnemy, this.player);
-    });
